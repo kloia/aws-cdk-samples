5613591722ad30db3b1ca681605c8a8ffd0f6937
Serkan Bingöl <serkanbingol@outlook.com>
2021-03-19 09:07:20 +0300

    cdk csharp eks-sample added

---
+++ csharp/eks-sample/src/EksSample/Constants.cs
@@ -0,0 +1,18 @@
+namespace EksCdk
+{
+    public class Constants
+    {
+        public static string ADMIN_ROLE = "AdminRole";
+
+        public static string STACK_PREFIX = "eks-cdk-";
+        public static string CLUSTER_ID = "cdk-eks";
+        public static string CLUSTER_NODE_GRP_ID = "nodegroup";
+        public static string EKS_SECURITY_GRP = "eks-sg";
+        public static string EC2_INSTANCE_TYPE = "t3.medium";
+        public static string EC2_INGRESS_DESCRIPTION = "EKS cluster Ingress";
+
+        public static string VPC_ID = "eks-vpc";
+
+        public static string VPC_CIDR = "10.0.0.0/16";
+    }
+}
---
+++ csharp/eks-sample/src/EksSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EksSample
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new EksSampleStack(app, "EksSampleStack");
+            app.Synth();
+        }
+    }
+}
